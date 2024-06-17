1703d4d8f1257abe58c667f37888c8293f580228
Prajwal T S <prajwal@zetapp.in>
2024-06-17 10:34:28 +0530

    add example

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,29 @@
 import { Text, View } from "react-native";
+import { XStack } from "../components/layout";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="gap-4 m-4">
+      {/* Does not work */}
+      <XStack>
+        <Text>Text1</Text>
+        <Text>Text2</Text>
+      </XStack>
+      {/* Does not work */}
+      <XStack className="flex-row">
+        <Text>Text1</Text>
+        <Text>Text2</Text>
+      </XStack>
+      {/* Works */}
+      <XStack style={{ flexDirection: "row" }}>
+        <Text>Text1</Text>
+        <Text>Text2</Text>
+      </XStack>
+      {/* Works */}
+      <View className="flex-row">
+        <Text>Text1</Text>
+        <Text>Text2</Text>
+      </View>
     </View>
   );
 }
---
+++ src/components/layout.tsx
@@ -0,0 +1,5 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {classed} from '@tw-classed/react'
+
+export const XStack = classed(View, 'flex-row')
